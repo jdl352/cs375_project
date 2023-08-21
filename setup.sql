d8aaf62bdbbe646cb7f51afd285771b448f2cf83
AkshitAnand <akshitanand1818@gmail.com>
2023-08-21 18:34:41 -0400

    Version changes for Iron_Session auth, login/out

--- setup.sql
+++ setup.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS projectdb;
 CREATE DATABASE projectdb;
 \c projectdb
-DROP TABLE IF EXISTS userLogin;
-CREATE TABLE userLogin(
+DROP TABLE IF EXISTS userlogin;
+CREATE TABLE userlogin(
 	id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE,
     password VARCHAR(100)
@@ -10,7 +10,7 @@ CREATE TABLE userLogin(
 DROP TABLE IF EXISTS threads;
 CREATE TABLE threads(
 	threadID SERIAL PRIMARY KEY,
-    username VARCHAR(50) REFERENCES userLogin(username),
+    username VARCHAR(50) REFERENCES userlogin(username),
     messagebody VARCHAR(2000),
     dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -18,7 +18,7 @@ DROP TABLE IF EXISTS comment;
 CREATE TABLE comment(
 	commentID SERIAL PRIMARY KEY,
     threadID INT REFERENCES threads(threadID),
-    username VARCHAR(50) REFERENCES userLogin(username),
+    username VARCHAR(50) REFERENCES userlogin(username),
     comment VARCHAR(200),
     dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
