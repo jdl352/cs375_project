da1b3ab16fabebdd5fe9e96a9eaebcc53322f0e0
jdl352 <109162620+jdl352@users.noreply.github.com>
2023-08-22 15:28:18 -0400

    fix import paths

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -3,41 +3,47 @@ import { withIronSessionApiRoute } from "iron-session/next";
 
 let argon2 = require("argon2");
 
-let tokenStorage = require("../tokenStorage.json");
+let tokenStorage = require("./tokenStorage.json");
 
-export default withIronSessionApiRoute(async (req, res) => {
+export default withIronSessionApiRoute(
+  async (req, res) => {
     const { username, password } = await req.body;
     if (!username || !password) {
-        res.status(400).json({ message: "Username or Password not provided" });
+      res.status(400).json({ message: "Username or Password not provided" });
     }
-  
+
     try {
-        let result = await pool.query("SELECT password FROM userLogin WHERE username = $1", [username]);
+      let result = await pool.query(
+        "SELECT password FROM userLogin WHERE username = $1",
+        [username]
+      );
 
-        // username doesn't exist
-        if (result.rows.length === 0) {
-            return res.status(400).json({message: "Incorrect username or password"});
-        }
+      // username doesn't exist
+      if (result.rows.length === 0) {
+        return res
+          .status(400)
+          .json({ message: "Incorrect username or password" });
+      }
 
-        let hashed = result.rows[0].password;
+      let hashed = result.rows[0].password;
 
-        let verifyResult;
-        try {
-            verifyResult = await argon2.verify(hashed, password);
-        } catch (error) {
-            console.log("VERIFY FAILED", error);
-            return res.status(500).json({message: error.message});
-        }
+      let verifyResult;
+      try {
+        verifyResult = await argon2.verify(hashed, password);
+      } catch (error) {
+        console.log("VERIFY FAILED", error);
+        return res.status(500).json({ message: error.message });
+      }
 
-        if (!verifyResult) {
-            console.log("Credentials didn't match");
-            return res.status(400).json({message: "Credentials didn't match"});
-        }
-  
-        const user = { isLoggedIn: true, username: username };
-        req.session.user = user;
-        await req.session.save();
-        res.json(user);
+      if (!verifyResult) {
+        console.log("Credentials didn't match");
+        return res.status(400).json({ message: "Credentials didn't match" });
+      }
+
+      const user = { isLoggedIn: true, username: username };
+      req.session.user = user;
+      await req.session.save();
+      res.json(user);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -49,4 +55,5 @@ export default withIronSessionApiRoute(async (req, res) => {
     cookieOptions: {
       secure: process.env.NODE_ENV === "production",
     },
-  });
+  }
+);
