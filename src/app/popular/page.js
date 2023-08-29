fd9cb3a84fdc37533cdfb60467c5afded174b8db
Zheren Gu <zherengu777@gmail.com>
2023-08-28 22:22:05 -0400

    Fixed Page Reload issue

--- src/app/popular/page.js
+++ src/app/popular/page.js
@@ -2,81 +2,33 @@ import Link from "next/link";
 import styles from "./popular.module.css";
 const BASE_URL = "https://newsapi.org/v2";
 import React from "react";
-
-const NewsAPI = require("newsapi");
-const newsapi = new NewsAPI("f523c32aa31c4dafa3ee1f62f6890100");
-
-let articles = [
-  {
-    name: "Article 1",
-    date: "20230814",
-    category: "Science",
-    id: 1,
-    likes: 10,
-  },
-  {
-    name: "Article 2",
-    date: "20230810",
-    category: "Politics",
-    id: 2,
-    likes: 20,
-  },
-  {
-    name: "Article 3",
-    date: "20230728",
-    category: "Environment",
-    id: 3,
-    likes: 5,
-  },
-];
-
-newsapi.v2
-  .topHeadlines({
-    language: "en",
-    pageSize: 20,
-  })
-  .then((response) => {
-    for (let x = 0; x < 20; x++) {
-      //20 is based on pageSize
-      console.log(response.articles[x]);
-      articles.push({
-        name: response.articles[x].source.name,
-        date: response.articles[x].publishedAt,
-        category: "Business",
-        id: x + 4,
-        url: response.articles[x].url,
-        likes: 7,
-      });
-    }
-  });
-
-articles.sort((a, b) => b.likes - a.likes);
-
-export default function Popular() {
 const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('API KEY');
-
+const newsapi = new NewsAPI('API_KEY');
+let articles = [];
 newsapi.v2.topHeadlines({
   category: 'business',
   language: 'en',
   pageSize: 20
 }).then(response => {
-
+  articles = [];
   for(let x = 0; x < 20; x++){ //20 is based on pageSize
     // console.log(x);
     articles.push({
     name:response.articles[x].source.name,
     date:response.articles[x].publishedAt,
     category:'Business',
-    id: x+4,
+    id: x,
     url: response.articles[x].url,
-    likes: 7 });
+    likes: 0 });
   }
 
 });
 
 
+export default function Popular() {
+
   const articleRows = articles.map((article) => (
+    
     <tr key={article.id}>
       <td>
         <Link href={"/article?id=" + article.id}>{article.name}</Link>
@@ -86,6 +38,7 @@ newsapi.v2.topHeadlines({
     </tr>
   ));
   return (
+    <body>
       <table className={styles.popularTable}>
         <thead>
           <td>Name</td>
