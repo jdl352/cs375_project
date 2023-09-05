66a83bcf059304c1ee836e66adc59f1195d24d87
jdl352 <109162620+jdl352@users.noreply.github.com>
2023-09-05 12:29:55 -0400

    log db query

--- src/app/threads/api/route.js
+++ src/app/threads/api/route.js
@@ -2,10 +2,8 @@ import pool from "../../../components/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  let result;
   try {
-    result = await pool.query("SELECT * FROM threads");
-    console.log(result);
+    let result = await pool.query("SELECT * FROM threads;");
     return NextResponse.json({ ok: true, result: result }, { status: 200 });
   } catch (error) {
     console.log("UPDATE FAILED", error);
--- src/app/threads/getThreads/getThreads.js
+++ src/app/threads/getThreads/getThreads.js
@@ -7,6 +7,7 @@ export function getThreads() {
   })
     .then((res) => res.json())
     .then((res) => {
+      console.log(res);
       let threadData = res.result.rows;
       let rows = threadData.map((thread) => (
         <ThreadRow
