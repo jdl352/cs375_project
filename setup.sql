5b881c9847d5d8b5ac9dc1f380394cac08d2e41d
AkshitAnand <akshitanand1818@gmail.com>
2023-09-05 12:12:02 -0400

    setup

--- setup.sql
+++ setup.sql
@@ -34,19 +34,19 @@ INSERT INTO userlogin (username, password) VALUES ('user5', 'password');
 INSERT INTO userlogin (username, password) VALUES ('user6', 'password');
 
 
-INSERT INTO threads (username, messagebody) VALUES ('user1', 'user1body1');
-INSERT INTO threads (username, messagebody) VALUES ('user2', 'user2body1');
-INSERT INTO threads (username, messagebody) VALUES ('user3', 'user3body1');
-INSERT INTO threads (username, messagebody) VALUES ('user4', 'user4body1');
-INSERT INTO threads (username, messagebody) VALUES ('user5', 'user5body1');
-INSERT INTO threads (username, messagebody) VALUES ('user6', 'user6body1');
+INSERT INTO threads (username, messagebody, url) VALUES ('user1', 'user1body1', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user2', 'user2body1', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user3', 'user3body1', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user4', 'user4body1', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user5', 'user5body1', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user6', 'user6body1', 'dummyurl');
 
-INSERT INTO threads (username, messagebody) VALUES ('user1', 'user1body2');
-INSERT INTO threads (username, messagebody) VALUES ('user2', 'user2body2');
-INSERT INTO threads (username, messagebody) VALUES ('user3', 'user3body2');
-INSERT INTO threads (username, messagebody) VALUES ('user4', 'user4body2');
-INSERT INTO threads (username, messagebody) VALUES ('user5', 'user5body2');
-INSERT INTO threads (username, messagebody) VALUES ('user6', 'user6body2');
+INSERT INTO threads (username, messagebody, url) VALUES ('user1', 'user1body2', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user2', 'user2body2', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user3', 'user3body2', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user4', 'user4body2', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user5', 'user5body2', 'dummyurl');
+INSERT INTO threads (username, messagebody, url) VALUES ('user6', 'user6body2', 'dummyurl');
 
 INSERT INTO comments (threadid, username, comment) VALUES (1, 'user2', 'user2comment');
 INSERT INTO comments (threadid, username, comment) VALUES (1, 'user4', 'user4comment');
