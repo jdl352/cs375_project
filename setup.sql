91b09de1e565a66ac79cbcdb33d689c781f47818
AkshitAnand <akshitanand1818@gmail.com>
2023-09-05 12:05:23 -0400

    DUMMYDATA

--- setup.sql
+++ setup.sql
@@ -26,3 +26,27 @@ CREATE TABLE comments(
     comment VARCHAR(200),
     dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+INSERT INTO userlogin (username, password) VALUES ('user1', 'password');
+INSERT INTO userlogin (username, password) VALUES ('user2', 'password');
+INSERT INTO userlogin (username, password) VALUES ('user3', 'password');
+INSERT INTO userlogin (username, password) VALUES ('user4', 'password');
+INSERT INTO userlogin (username, password) VALUES ('user5', 'password');
+INSERT INTO userlogin (username, password) VALUES ('user6', 'password');
+
+
+INSERT INTO threads (username, messagebody) VALUES ('user1', 'user1body1');
+INSERT INTO threads (username, messagebody) VALUES ('user2', 'user2body1');
+INSERT INTO threads (username, messagebody) VALUES ('user3', 'user3body1');
+INSERT INTO threads (username, messagebody) VALUES ('user4', 'user4body1');
+INSERT INTO threads (username, messagebody) VALUES ('user5', 'user5body1');
+INSERT INTO threads (username, messagebody) VALUES ('user6', 'user6body1');
+
+INSERT INTO threads (username, messagebody) VALUES ('user1', 'user1body2');
+INSERT INTO threads (username, messagebody) VALUES ('user2', 'user2body2');
+INSERT INTO threads (username, messagebody) VALUES ('user3', 'user3body2');
+INSERT INTO threads (username, messagebody) VALUES ('user4', 'user4body2');
+INSERT INTO threads (username, messagebody) VALUES ('user5', 'user5body2');
+INSERT INTO threads (username, messagebody) VALUES ('user6', 'user6body2');
+
+INSERT INTO comments (threadid, username, comment) VALUES (1, 'user2', 'user2comment');
+INSERT INTO comments (threadid, username, comment) VALUES (1, 'user4', 'user4comment');
