ea93e581880391c0a1165a10b7ea080afeb3c8f6
AkshitAnand <akshitanand1818@gmail.com>
2023-09-05 12:17:27 -0400

    homepage simple text

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        This is where we can have an overview
+        Welcome to the Landing Page!
       </div>
     </main>
   );
